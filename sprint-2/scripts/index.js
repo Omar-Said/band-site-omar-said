9d8132be7fc09b2a161c6f0ae09a4a897a1f18f6
Omar-Said <omar.a.said@outlook.com>
2020-10-30 19:25:24 -0400

    JS + API configuration complete

--- sprint-2/scripts/index.js
+++ sprint-2/scripts/index.js
@@ -1,101 +1,236 @@
-let commentList = [
-    {
-    name: "Micheal Lyons",
-    comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-    date: "12/18/2018"
-},
-{
-    name: "Gary Wong",
-    comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!.",
-    date: "12/12/2018"
-},
-{
-    name: "Theodore Duncan",
-    comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-    date: "11/15/2018"
+
+// API key
+
+let apiKeyAuth = "?api_key=ecb2eb23-84b4-4049-a3b3-5ac139a4c86f"; 
+
+// API get request for default comments
+
+const getComments = () => {axios.get('https://project-1-api.herokuapp.com/comments/' + apiKeyAuth)
+.then((response) => {
+
+    commentListArr = response.data;
+
+    publishComments(commentListArr);
+
+    }).catch ((error) => {
+        console.log(error);
+    })
 }
 
-];
+// API iterator
 
-window.addEventListener('load', publishComments(commentList));
+let commentContainer = document.querySelector('.commented__container');
+
+function publishComments(commentListArr) {
+    let commentContainer = document.querySelector('.commented__container');
+
+    commentContainer.innerHTML = '';
+
+    commentListArr.sort((a, b) => b.timestamp - a.timestamp).forEach((element, i) => {
+        displayComments(commentListArr[i]);
+    });
+}
+
+// DOM appending function
+
+function displayComments(comment){ 
+    
+    let commentContainer = document.querySelector('.commented__container');
+    
+        let divEl = document.createElement('div');
+        divEl.classList.add('commented__wrapper-left');
+        
+        let emptyDiv = document.createElement('div');
+        divEl.appendChild(emptyDiv);
+        
+        let imgEl = document.createElement('div');
+        imgEl.classList.add('commented__img', 'commented__avatar')
+        emptyDiv.appendChild(imgEl);
+        
+        let wrapEl = document.createElement('div');
+         wrapEl.classList.add('commented__wrapper')
+        divEl.appendChild(wrapEl);
+        
+        let wrapTopEl = document.createElement('div');
+        wrapTopEl.classList.add('commented__wrapper-top')
+        wrapEl.appendChild(wrapTopEl);
+        
+        let nameEL = document.createElement('p');
+        nameEL.classList.add('commented__name');
+        wrapTopEl.appendChild(nameEL);
+        
+        let dateEl = document.createElement('p');
+        dateEl.classList.add('commented__date');
+        wrapTopEl.appendChild(dateEl);
+        
+        let commentEl = document.createElement('p');
+        commentEl.classList.add('commented__comment');
+        wrapEl.appendChild(commentEl);
+        
+        nameEL.innerText = comment.name;
+        commentEl.innerText = comment.comment;
+        dateEl.innerText =  new Date (comment.timestamp).toLocaleDateString();
+
+        commentContainer.appendChild(divEl); 
+}; 
+   
+// Post request headers
+
+const header = {
+    'Content-Type': 'application/json'
+};
+
+
+//  Form submit function handler
 
 let formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', handleFormSubmit);
 
+const formValue = {};
 
 function handleFormSubmit(event) {
     event.preventDefault();
 
-    let comment__nameVal = event.target.comment__name.value;
-    let comment__commentVal = event.target.comment__comment.value;
+    let commentName = event.target.comment__name.value;
+    let commentVal = event.target.comment__comment.value;
+    formValue.name = commentName;
+    formValue.comment = commentVal;
 
-    commentList.unshift({
-        name: comment__nameVal,
-        comment: comment__commentVal,
-        date: new Date().toLocaleDateString()
-})
-    publishComments(commentList);
-    formEl.reset();
+    post();
 }
 
+// Post request
 
-function publishComments(commentList) {
-    let commentContainer = document.querySelector('.commented__container');
-
+const post = () => 
+{axios.post('https://project-1-api.herokuapp.com/comments/' + apiKeyAuth, formValue, header)
+.then((response) => {
     commentContainer.innerHTML = '';
+    formEl.reset();
+    getComments();
+    })
+}
 
-    for (let i = 0; i < commentList.length; i++) {
+getComments();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Working code from Spint 2
+
+
+
+// let commentList = [
+//     {
+//     name: "Micheal Lyons",
+//     comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+//     date: "12/18/2018"
+// },
+// {
+//     name: "Gary Wong",
+//     comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!.",
+//     date: "12/12/2018"
+// },
+// {
+//     name: "Theodore Duncan",
+//     comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+//     date: "11/15/2018"
+// }
+
+// ];
+
+// window.addEventListener('load', publishComments(commentList));
+
+// let formEl = document.querySelector('.form');
+
+// formEl.addEventListener('submit', handleFormSubmit);
+
+
+// function handleFormSubmit(event) {
+//     event.preventDefault();
+
+//     let comment__nameVal = event.target.comment__name.value;
+//     let comment__commentVal = event.target.comment__comment.value;
+
+//     commentList.unshift({
+//         name: comment__nameVal,
+//         comment: comment__commentVal,
+//         date: new Date().toLocaleDateString()
+// })
+//     publishComments(commentList);
+//     formEl.reset();
+// }
+
+
+// function publishComments(commentList) {
+//     let commentContainer = document.querySelector('.commented__container');
+
+//     commentContainer.innerHTML = '';
+
+//     for (let i = 0; i < commentList.length; i++) {
         
-        displayComment(commentList[i]);
+//         displayComment(commentList[i]);
 
-    }
+//     }
 
-}
+// }
 
-function displayComment(comment) {
+// function displayComment(comment) {
 
-    let commentContainer = document.querySelector('.commented__container');
+//     let commentContainer = document.querySelector('.commented__container');
 
-        let divEl = document.createElement('div');
-        divEl.classList.add('commented__wrapper-left');
+//         let divEl = document.createElement('div');
+//         divEl.classList.add('commented__wrapper-left');
 
-        let emptyDiv = document.createElement('div');
-        divEl.appendChild(emptyDiv);
+//         let emptyDiv = document.createElement('div');
+//         divEl.appendChild(emptyDiv);
 
-        let imgEl = document.createElement('div');
-        imgEl.classList.add('commented__img', 'commented__avatar')
-        emptyDiv.appendChild(imgEl);
+//         let imgEl = document.createElement('div');
+//         imgEl.classList.add('commented__img', 'commented__avatar')
+//         emptyDiv.appendChild(imgEl);
 
-        let wrapEl = document.createElement('div');
-        wrapEl.classList.add('commented__wrapper')
-        divEl.appendChild(wrapEl);
-
-
-        let wrapTopEl = document.createElement('div');
-        wrapTopEl.classList.add('commented__wrapper-top')
-        wrapEl.appendChild(wrapTopEl);
+//         let wrapEl = document.createElement('div');
+//         wrapEl.classList.add('commented__wrapper')
+//         divEl.appendChild(wrapEl);
 
 
-        let nameEL = document.createElement('p');
-        nameEL.classList.add('commented__name');
-        wrapTopEl.appendChild(nameEL);
+//         let wrapTopEl = document.createElement('div');
+//         wrapTopEl.classList.add('commented__wrapper-top')
+//         wrapEl.appendChild(wrapTopEl);
 
 
-        let dateEl = document.createElement('p');
-        dateEl.classList.add('commented__date');
-        wrapTopEl.appendChild(dateEl);
+//         let nameEL = document.createElement('p');
+//         nameEL.classList.add('commented__name');
+//         wrapTopEl.appendChild(nameEL);
 
 
-        let commentEl = document.createElement('p');
-        commentEl.classList.add('commented__comment');
-        wrapEl.appendChild(commentEl);
+//         let dateEl = document.createElement('p');
+//         dateEl.classList.add('commented__date');
+//         wrapTopEl.appendChild(dateEl);
 
-        nameEL.innerText = comment.name;
-        commentEl.innerText = comment.comment;
-        dateEl.innerText = comment.date;
 
-        commentContainer.appendChild(divEl); 
+//         let commentEl = document.createElement('p');
+//         commentEl.classList.add('commented__comment');
+//         wrapEl.appendChild(commentEl);
 
-}
+//         nameEL.innerText = comment.name;
+//         commentEl.innerText = comment.comment;
+//         dateEl.innerText = comment.date;
 
+//         commentContainer.appendChild(divEl); 
+
+// }
--- sprint-2/scripts/shows.js
+++ sprint-2/scripts/shows.js
@@ -1,70 +1,100 @@
+
+// API get request API authorization key
+
 axios.get('https://project-1-api.herokuapp.com/register')
-    .then((result) => {
-        let apiKey = result;
+    .then((response) => {
+
+        let apiKey = response;
+
     }).catch((error) => {
+
         console.error(error);
+        
     })
 
+// API authorization key
 
 let apiKeyAuth = "?api_key=ecb2eb23-84b4-4049-a3b3-5ac139a4c86f"; 
 
+//  API get request for Band's show list
 
 axios.get('https://project-1-api.herokuapp.com/showdates/' + apiKeyAuth) 
     .then ((response) => {
 
-        let showEl = document.querySelector('.shows__container');
-        
         showListArr = response.data;
 
-        for (let i = 0; i < showListArr.length; i++) {
+        publishShows(showListArr)
 
-    let divEl = document.createElement('div');
-    divEl.classList.add('shows__container-wrapper')
+    }).catch((error) => {
 
-    let dateEL = document.createElement('p');
-    dateEL.classList.add('shows__container-header');
-    dateEL.innerHTML = "DATE";
-    divEl.appendChild(dateEL);
+         console.log(error);
 
-    let date = document.createElement('p');
-    date.classList.add('shows__container-info');
-    divEl.appendChild(date);
-
-    let venueEl = document.createElement('p');
-    venueEl.classList.add('shows__container-header')
-    venueEl.innerHTML = "VENUE";
-    divEl.appendChild(venueEl);
-
-    let venue = document.createElement('p');
-    venue.classList.add('shows__container-info')
-    divEl.appendChild(venue);
-
-    let locEl = document.createElement('p');
-    locEl.classList.add('shows__container-header')
-    locEl.innerHTML = "LOCATION"
-    divEl.appendChild(locEl);
-
-    let location = document.createElement('p');
-    location.classList.add('shows__container-info')
-    divEl.appendChild(location);
-
-    let submit = document.createElement('input');
-    submit.classList.add('shows__container-button')
-    submit.setAttribute('type', 'submit')
-    submit.setAttribute('value', 'BUY TICKETS')
-    divEl.appendChild(submit);
-
-
-
-    date.innerText = showListArr[i].date;
-    venue.innerText = showListArr[i].place;
-    location.innerText = showListArr[i].location;
-
-    showEl.appendChild(divEl);
-
-                }
     })
 
+// API Iterator
+
+function publishShows(showListArr) {
+    let commentContainer = document.querySelector('.commented__container');
+
+    showListArr.forEach((element, i) => {
+        displayShows(showListArr[i]);
+    });
+}
+
+// For each function to append DOM with show times
+
+function displayShows(shows){
+
+    let showEl = document.querySelector('.shows__container');
+        
+        let divEl = document.createElement('div');
+        divEl.classList.add('shows__container-wrapper')
+
+        let dateEL = document.createElement('p');
+        dateEL.classList.add('shows__container-header');
+        dateEL.innerHTML = "DATE";
+        divEl.appendChild(dateEL);
+
+        let date = document.createElement('p');
+        date.classList.add('shows__container-info');
+        divEl.appendChild(date);
+
+        let venueEl = document.createElement('p');
+        venueEl.classList.add('shows__container-header')
+        venueEl.innerHTML = "VENUE";
+        divEl.appendChild(venueEl);
+
+        let venue = document.createElement('p');
+        venue.classList.add('shows__container-info')
+        divEl.appendChild(venue);
+
+        let locEl = document.createElement('p');
+        locEl.classList.add('shows__container-header')
+        locEl.innerHTML = "LOCATION"
+        divEl.appendChild(locEl);
+
+        let location = document.createElement('p');
+        location.classList.add('shows__container-info')
+        divEl.appendChild(location);
+
+        let submit = document.createElement('input');
+        submit.classList.add('shows__container-button')
+        submit.setAttribute('type', 'submit')
+        submit.setAttribute('value', 'BUY TICKETS')
+        divEl.appendChild(submit);
+
+        date.innerText = shows.date;
+        venue.innerText = shows.place;
+        location.innerText = shows.location;
+
+        showEl.appendChild(divEl);
+ }
+
+
+
+
+
+    
 //  Previous code for reference
 
 // let showList = [{
