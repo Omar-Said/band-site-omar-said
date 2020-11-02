82ade10a921756b2a703a554833826b9543b1443
Omar-Said <omar.a.said@outlook.com>
2020-11-01 21:37:10 -0500

    Final Commit

--- sprint-2/scripts/index.js
+++ sprint-2/scripts/index.js
@@ -1,4 +1,3 @@
-
 // API key
 
 let apiKeyAuth = "?api_key=ecb2eb23-84b4-4049-a3b3-5ac139a4c86f"; 
@@ -27,7 +26,9 @@ function publishComments(commentListArr) {
     commentContainer.innerHTML = '';
 
     commentListArr.sort((a, b) => b.timestamp - a.timestamp).forEach((element, i) => {
+        
         displayComments(commentListArr[i]);
+
     });
 }
 
@@ -80,7 +81,6 @@ const header = {
     'Content-Type': 'application/json'
 };
 
-
 //  Form submit function handler
 
 let formEl = document.querySelector('.form');
@@ -94,6 +94,7 @@ function handleFormSubmit(event) {
 
     let commentName = event.target.comment__name.value;
     let commentVal = event.target.comment__comment.value;
+
     formValue.name = commentName;
     formValue.comment = commentVal;
 
@@ -105,132 +106,12 @@ function handleFormSubmit(event) {
 const post = () => 
 {axios.post('https://project-1-api.herokuapp.com/comments/' + apiKeyAuth, formValue, header)
 .then((response) => {
-    commentContainer.innerHTML = '';
-    formEl.reset();
-    getComments();
+
+        commentContainer.innerHTML = '';
+        formEl.reset();
+        getComments();
+
     })
 }
 
 getComments();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Working code from Spint 2
-
-
-
-// let commentList = [
-//     {
-//     name: "Micheal Lyons",
-//     comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-//     date: "12/18/2018"
-// },
-// {
-//     name: "Gary Wong",
-//     comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!.",
-//     date: "12/12/2018"
-// },
-// {
-//     name: "Theodore Duncan",
-//     comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-//     date: "11/15/2018"
-// }
-
-// ];
-
-// window.addEventListener('load', publishComments(commentList));
-
-// let formEl = document.querySelector('.form');
-
-// formEl.addEventListener('submit', handleFormSubmit);
-
-
-// function handleFormSubmit(event) {
-//     event.preventDefault();
-
-//     let comment__nameVal = event.target.comment__name.value;
-//     let comment__commentVal = event.target.comment__comment.value;
-
-//     commentList.unshift({
-//         name: comment__nameVal,
-//         comment: comment__commentVal,
-//         date: new Date().toLocaleDateString()
-// })
-//     publishComments(commentList);
-//     formEl.reset();
-// }
-
-
-// function publishComments(commentList) {
-//     let commentContainer = document.querySelector('.commented__container');
-
-//     commentContainer.innerHTML = '';
-
-//     for (let i = 0; i < commentList.length; i++) {
-        
-//         displayComment(commentList[i]);
-
-//     }
-
-// }
-
-// function displayComment(comment) {
-
-//     let commentContainer = document.querySelector('.commented__container');
-
-//         let divEl = document.createElement('div');
-//         divEl.classList.add('commented__wrapper-left');
-
-//         let emptyDiv = document.createElement('div');
-//         divEl.appendChild(emptyDiv);
-
-//         let imgEl = document.createElement('div');
-//         imgEl.classList.add('commented__img', 'commented__avatar')
-//         emptyDiv.appendChild(imgEl);
-
-//         let wrapEl = document.createElement('div');
-//         wrapEl.classList.add('commented__wrapper')
-//         divEl.appendChild(wrapEl);
-
-
-//         let wrapTopEl = document.createElement('div');
-//         wrapTopEl.classList.add('commented__wrapper-top')
-//         wrapEl.appendChild(wrapTopEl);
-
-
-//         let nameEL = document.createElement('p');
-//         nameEL.classList.add('commented__name');
-//         wrapTopEl.appendChild(nameEL);
-
-
-//         let dateEl = document.createElement('p');
-//         dateEl.classList.add('commented__date');
-//         wrapTopEl.appendChild(dateEl);
-
-
-//         let commentEl = document.createElement('p');
-//         commentEl.classList.add('commented__comment');
-//         wrapEl.appendChild(commentEl);
-
-//         nameEL.innerText = comment.name;
-//         commentEl.innerText = comment.comment;
-//         dateEl.innerText = comment.date;
-
-//         commentContainer.appendChild(divEl); 
-
-// }
--- sprint-2/scripts/shows.js
+++ sprint-2/scripts/shows.js
@@ -1,4 +1,3 @@
-
 // API get request API authorization key
 
 axios.get('https://project-1-api.herokuapp.com/register')
@@ -34,14 +33,17 @@ axios.get('https://project-1-api.herokuapp.com/showdates/' + apiKeyAuth)
 // API Iterator
 
 function publishShows(showListArr) {
+
     let commentContainer = document.querySelector('.commented__container');
 
     showListArr.forEach((element, i) => {
+
         displayShows(showListArr[i]);
+
     });
 }
 
-// For each function to append DOM with show times
+// DOM appending function
 
 function displayShows(shows){
 
@@ -51,34 +53,34 @@ function displayShows(shows){
         divEl.classList.add('shows__container-wrapper')
 
         let dateEL = document.createElement('p');
-        dateEL.classList.add('shows__container-header');
+        dateEL.classList.add('shows__header');
         dateEL.innerHTML = "DATE";
         divEl.appendChild(dateEL);
 
         let date = document.createElement('p');
-        date.classList.add('shows__container-info');
+        date.classList.add('shows__info');
         divEl.appendChild(date);
 
         let venueEl = document.createElement('p');
-        venueEl.classList.add('shows__container-header')
+        venueEl.classList.add('shows__header')
         venueEl.innerHTML = "VENUE";
         divEl.appendChild(venueEl);
 
         let venue = document.createElement('p');
-        venue.classList.add('shows__container-info')
+        venue.classList.add('shows__info')
         divEl.appendChild(venue);
 
         let locEl = document.createElement('p');
-        locEl.classList.add('shows__container-header')
+        locEl.classList.add('shows__header')
         locEl.innerHTML = "LOCATION"
         divEl.appendChild(locEl);
 
         let location = document.createElement('p');
-        location.classList.add('shows__container-info')
+        location.classList.add('shows__info')
         divEl.appendChild(location);
 
         let submit = document.createElement('input');
-        submit.classList.add('shows__container-button')
+        submit.classList.add('shows__button')
         submit.setAttribute('type', 'submit')
         submit.setAttribute('value', 'BUY TICKETS')
         divEl.appendChild(submit);
@@ -89,122 +91,3 @@ function displayShows(shows){
 
         showEl.appendChild(divEl);
  }
-
-
-
-
-
-    
-//  Previous code for reference
-
-// let showList = [{
-//     dateHeader: "DATE",
-//     date: "Mon Dec 17 2018",
-//     venueHeader: "VENUE",
-//     venue: "Ronald Lane",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// {
-//     dateHeader: "DATE",
-//     date: "Tue Jul 18 2019",
-//     venueHeader: "VENUE",
-//     venue: "Pier 3 East",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// {
-//     dateHeader: "DATE",
-//     date: "Fri Jul 22 2019",
-//     venueHeader: "VENUE",
-//     venue: "View Loungue",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// {
-//     dateHeader: "DATE",
-//     date: "Sat Aug 12 2019",
-//     venueHeader: "VENUE",
-//     venue: "Hyatt Agency",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// {
-//     dateHeader: "DATE",
-//     date: "Fri Sep 05 2019",
-//     venueHeader: "VENUE",
-//     venue: "Moscow Center",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// {
-//     dateHeader: "DATE",
-//     date: "Wed Aug 11 2019",
-//     venueHeader: "VENUE",
-//     venue: "Pres Club",
-//     locHeader: 'LOCATION',
-//     location: "San Fancisco, CA"
-// },
-// ]
-
-// window.addEventListener('load', publishShows(showList));
-
-// function publishShows(showList) {
-//     let showEl = document.querySelector('.shows__container');
-
-//     showEl.innerHTML = '';
-
-//     for (let i = 0; i < showList.length; i++) {
-//         displayShows(showList[i]);
-//     }
-// }
-
-// function displayShows(shows){
-    
-//     let showEl = document.querySelector('.shows__container');
-
-//     let divEl = document.createElement('div');
-//     divEl.classList.add('shows__container-wrapper')
-
-//     let dateEL = document.createElement('p');
-//     dateEL.classList.add('shows__container-header');
-//     divEl.appendChild(dateEL);
-
-//     let date = document.createElement('p');
-//     date.classList.add('shows__container-info');
-//     divEl.appendChild(date);
-
-//     let venueEl = document.createElement('p');
-//     venueEl.classList.add('shows__container-header')
-//     divEl.appendChild(venueEl);
-
-//     let venue = document.createElement('p');
-//     venue.classList.add('shows__container-info')
-//     divEl.appendChild(venue);
-
-//     let locEl = document.createElement('p');
-//     locEl.classList.add('shows__container-header')
-//     divEl.appendChild(locEl);
-
-//     let location = document.createElement('p');
-//     location.classList.add('shows__container-info')
-//     divEl.appendChild(location);
-
-//     let submit = document.createElement('input');
-//     submit.classList.add('shows__container-button')
-//     submit.setAttribute('type', 'submit')
-//     submit.setAttribute('value', 'BUY TICKETS')
-//     divEl.appendChild(submit);
-
-//     dateEL.innerText = shows.dateHeader;
-//     date.innerText = shows.date;
-
-//     venueEl.innerText = shows.venueHeader;
-//     venue.innerText = shows.venue;
-
-//     locEl.innerText = shows.locHeader,
-//     location.innerText = shows.location;
-
-//     showEl.appendChild(divEl);
-
-// };
